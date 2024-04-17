9967c53fd6df6fc3df41f65d7803addec73479d1
kabilMurugesan <98404076+kabilMurugesan@users.noreply.github.com>
2024-04-17 12:00:34 +0530

    test commit

--- testing/src/App.js
+++ testing/src/App.js
@@ -2,41 +2,42 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  // const [data, setData] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
-  const fetchData = () => {
-    axios.get('http://localhost:4000')
-      .then(response => {
-        setData(response.data);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch(error => {
-        setData(null);
-        setIsLoading(false);
-        setError('Error fetching data');
-      });
-  };
+  // const fetchData = () => {
+  //   axios.get('http://localhost:4000')
+  //     .then(response => {
+  //       setData(response.data);
+  //       setIsLoading(false);
+  //       setError(null);
+  //     })
+  //     .catch(error => {
+  //       setData(null);
+  //       setIsLoading(false);
+  //       setError('Error fetching data');
+  //     });
+  // };
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+  // if (isLoading) {
+  //   return <p>Loading...</p>;
+  // }
 
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
+  // if (error) {
+  //   return <p>Error: {error}</p>;
+  // }
 
   return (
-    <div>
-      {/* Render your fetched data here */}
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    // <div>
+    //   {/* Render your fetched data here */}
+    //   <pre>{JSON.stringify(data, null, 2)}</pre>
+    // </div>
+    <p>welcome</p>
   );
 }
 
